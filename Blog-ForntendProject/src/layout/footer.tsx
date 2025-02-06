b7c3665d1900ade713c2721a5c77483f79248da8
Rashmi Sharmila Fernando <108237108+rashmisharmilafernando@users.noreply.github.com>
2025-02-06 16:38:09 +0530

    Builded header and footer

---
+++ Blog-ForntendProject/src/layout/footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Footer extends React.Component<any, any> {
+
+    render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | Iterable<React.ReactNode> | React.ReactPortal | boolean | any | null | undefined {
+        return (
+            <footer className={'bg-cyan-950 text-white p-5'}>
+                <h1 className={'text-[#0A3981] text-3xl font-semibold'}>BLOG</h1>
+                
+                <div className={'mt-5 text-center'}>Copyright © 2025 Blog LK</div>
+            </footer>
+        );
+    }
+
+}
+
+export default Footer
---
+++ Blog-ForntendProject/src/layout/header.tsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import { Link } from "react-router-dom";
+
+class Header extends React.Component<any, any> {
+
+  render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | Iterable<React.ReactNode> | React.ReactPortal | boolean | any | null | undefined {
+    return(
+      <header>
+        <nav className={'flex justify-between items-center p-5'}>
+          <h1 className={'text-[#0A3981] text-3xl font-semibold' }>BLOG</h1>  
+          <ul className={'flex gap-[4vw]'}>
+            <li> <Link to="/Home">Blogs</Link></li>
+            <li><Link to="/MyBlogs">Your Blogs</Link></li>
+            <li><Link to="/NewBlogPost">New Blog Post</Link></li>
+            <li className='text-black  hover:text-[#0A3981] font-semibold '><Link to="/Login">Login</Link></li>
+            <li className='text-black  hover:text-[#0A3981] font-semibold'><Link to="/Signup">Create Account</Link></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+
+}
+
+export default Header;
